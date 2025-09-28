32d4ffbcb64ea25b462b9141a61134a951925991
Asher Hoeveler <dev@crinfarr.io>
2025-09-28 01:20:12 -0500

    more bullshit

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ mod pack_meta;
 
 use std::{
     fs::File,
-    io::{self},
+    io::{self}, sync::Arc,
 };
 
-use tokio::sync::Semaphore;
-use tracing::{Level, event};
-use zip::{read::ZipFile, result::ZipResult};
+use tokio::{sync::{Mutex, Semaphore}, task::JoinSet};
+use tracing::{event, span, Instrument, Level};
+use zip::{read::ZipFile, result::ZipResult, ZipArchive};
 
-use crate::pack_meta::{CurseforgeMeta, ModrinthMeta};
+use crate::pack_meta::{CurseGetModResponse, CurseforgeMeta, ModrinthMeta};
 enum PackMeta {
     Modrinth(ModrinthMeta),
     Curse(CurseforgeMeta),
@@ -31,6 +31,12 @@ async fn main() -> color_eyre::Result<()> {
     event!(Level::DEBUG, "Opening {}", pack_filename);
 
     let mut archive = zip::ZipArchive::new(i_file)?;
+    
+    let sem_max_concurrent_downloads = Arc::new(Semaphore::new(100));
+    let client = Arc::new(reqwest::Client::builder()
+        .user_agent("io.crinfarr.Multipack")
+        .build()?);
+    
     for idx in 0..archive.len() {
         let file = archive.name_for_index(idx).ok_or(
             io::Error::new(io::ErrorKind::InvalidData, format!("Could not get file at index {idx}"))
@@ -40,13 +46,22 @@ async fn main() -> color_eyre::Result<()> {
             //TODO MODRINTH METADATA TRANSLATION
         } else if file == "manifest.json" {
             let curse_config = serde_json::from_reader::<ZipFile<File>, CurseforgeMeta>(archive.by_index(idx)?)?;
-            //TODO CURSE METADATA TRANSLATION
+            let mut handle_set:JoinSet<()> = JoinSet::new();
+            for file_stats in curse_config.files {
+                let sem_dl = sem_max_concurrent_downloads.clone();
+                let client_ref = client.clone();
+                let fsr = file_stats.clone();
+                handle_set.spawn(async move {
+                    event!(Level::DEBUG, "Waiting on semaphore");
+                    let permit = sem_dl.acquire().await.unwrap();
+                    let res = client_ref.get(format!("https://api.curseforge.com/v1/mods/{}", fsr.file_id)).send().await.unwrap();
+                    let modinfo = serde_json::from_str::<CurseGetModResponse>(&res.text().await.unwrap()).unwrap();
+                    drop(permit);
+                    
+                }.instrument(span!(Level::INFO, "Download Thread", FileID = file_stats.file_id)));
+            }
         }
     }
-    let sem_max_concurrent_downloads = Semaphore::new(100);
-    let client = reqwest::Client::builder()
-        .user_agent("io.crinfarr.Multipack")
-        .build()?;
 
     Ok(())
 }
--- src/pack_meta.rs
+++ src/pack_meta.rs
@@ -5,72 +5,401 @@ use serde::Deserialize;
 #[derive(Deserialize)]
 #[allow(unused)]
 pub struct ModrinthMeta {
-    game:String,
+    pub game: String,
     #[serde(rename = "formatVersion")]
-    format_version:u8,
+    pub format_version: u8,
     #[serde(rename = "versionId")]
-    version_id:String,
-    name:String,
-    summary:String,
-    files:Vec<ModrinthJarMeta>,
-    dependencies:BTreeMap<String, String>
+    pub version_id: String,
+    pub name: String,
+    pub summary: String,
+    pub files: Vec<ModrinthJarMeta>,
+    pub dependencies: BTreeMap<String, String>,
 }
 #[derive(Deserialize)]
 #[allow(unused)]
 pub struct ModrinthJarMeta {
-    path:String,
-    hashes: ModrinthJarFileHashes,
-    env: ModrinthEnvironmentRequirement,
-    downloads:Vec<String>,
+    pub path: String,
+    pub hashes: ModrinthJarFileHashes,
+    pub env: ModrinthEnvironmentRequirement,
+    pub downloads: Vec<String>,
     #[serde(rename = "fileSize")]
-    file_size:u32,
+    pub file_size: u32,
 }
 #[derive(Deserialize)]
 #[allow(unused)]
 pub struct ModrinthJarFileHashes {
-    sha512:String,
-    sha1:String,
+    pub sha512: String,
+    pub sha1: String,
 }
 #[derive(Deserialize)]
 #[allow(unused)]
 pub struct ModrinthEnvironmentRequirement {
-    client:String,
-    server:String
+    pub client: String,
+    pub server: String,
 }
 
 #[derive(Deserialize)]
 #[allow(unused)]
+#[serde(rename = "camelCase")]
 pub struct CurseforgeMeta {
-    minecraft:CurseMinecraftMetadata,
-    #[serde(rename = "manifestType")]
-    manifest_type:String,
-    #[serde(rename = "manifestVersion")]
-    manifest_version:u8,
-    name:String,
-    version:String,
-    author:String,
-    files:Vec<CurseFileDescription>,
-    overrides:String
+    pub minecraft: CurseMinecraftMetadata,
+    pub manifest_type: String,
+    pub manifest_version: u8,
+    pub name: String,
+    pub version: String,
+    pub author: String,
+    pub files: Vec<CurseFileDescription>,
+    pub overrides: String,
 }
 #[derive(Deserialize)]
 #[allow(unused)]
+#[serde(rename = "camelCase")]
 pub struct CurseMinecraftMetadata {
-    version:String,
-    #[serde(rename = "modLoaders")]
-    mod_loaders:Vec<CurseModLoaderMeta>,
+    pub version: String,
+    pub mod_loaders: Vec<CurseModLoaderMeta>,
 }
 #[derive(Deserialize)]
 #[allow(unused)]
 pub struct CurseModLoaderMeta {
-    id:String,
-    primary:bool
+    pub id: String,
+    pub primary: bool,
+}
+#[derive(Deserialize, Clone)]
+#[allow(unused)]
+#[serde(rename = "camelCase")]
+pub struct CurseFileDescription {
+    pub project_id: String,
+    pub file_id: String,
+    pub required: bool,
+}
+
+#[derive(Deserialize)]
+#[allow(unused)]
+pub struct CurseGetModResponse {
+    pub data: CurseMod,
 }
 #[derive(Deserialize)]
 #[allow(unused)]
-pub struct CurseFileDescription {
-    #[serde(rename = "projectID")]
-    project_id:String,
-    #[serde(rename = "fileID")]
-    file_id:String,
-    required:bool
+#[serde(rename = "camelCase")]
+pub struct CurseMod {
+    pub id: i32,
+    pub game_id: i32,
+    pub name: String,
+    pub slug: String,
+    pub links: CurseModLinks,
+    pub summary: String,
+    pub status: CurseModStatus,
+    pub download_count: i64,
+    pub is_featured: bool,
+    pub primary_category_id: i32,
+    pub categories: Vec<CurseCategory>,
+    pub class_id: Option<i32>,
+    pub authors: Vec<CurseModAuthor>,
+    pub logo: CurseModAsset,
+    pub screenshots: CurseModAsset,
+    pub main_file_id: i32,
+}
+#[derive(Deserialize)]
+#[allow(unused)]
+#[serde(rename = "camelCase")]
+pub struct CurseModLinks {
+    pub website_url: String,
+    pub wiki_url: String,
+    pub issues_url: String,
+    pub source_url: String,
+}
+#[derive(Clone)]
+pub enum CurseModStatus {
+    New,
+    ChangesRequired,
+    UnderSoftReview,
+    Approved,
+    Rejected,
+    ChangesMade,
+    Inactive,
+    Abandoned,
+    Deleted,
+    UnderReview,
+}
+impl<'de> Deserialize<'de> for CurseModStatus {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        match i32::deserialize(deserializer) {
+            Ok(i) => {
+                if i <= 10 && i >= 1 {
+                    Ok([
+                        Self::New,
+                        Self::ChangesRequired,
+                        Self::UnderSoftReview,
+                        Self::Approved,
+                        Self::Rejected,
+                        Self::ChangesMade,
+                        Self::Inactive,
+                        Self::Abandoned,
+                        Self::Deleted,
+                    ][i as usize - 1]
+                        .clone())
+                } else {
+                    Err(serde::de::Error::invalid_value(
+                        serde::de::Unexpected::Signed(i as i64),
+                        &"a number [1,10]",
+                    ))
+                }
+            }
+            Err(e) => Err(e),
+        }
+    }
+}
+#[derive(Deserialize)]
+#[allow(unused)]
+pub struct CurseCategory {
+    pub id: i32,
+    pub game_id: i32,
+    pub name: String,
+    pub slug: String,
+    pub url: String,
+    pub icon_url: String,
+    pub date_modified: String,
+    pub is_class: Option<bool>,
+    pub class_id: Option<i32>,
+    pub parent_category_id: Option<i32>,
+    pub display_index: Option<i32>,
+}
+#[derive(Deserialize)]
+#[allow(unused)]
+pub struct CurseModAuthor {
+    pub id: i32,
+    pub name: String,
+    pub url: String,
+}
+#[derive(Deserialize)]
+#[allow(unused)]
+pub struct CurseModAsset {
+    pub id: i32,
+    pub mod_id: i32,
+    pub title: String,
+    pub description: String,
+    pub thumbnail_url: String,
+    pub url: String,
+}
+#[derive(Deserialize)]
+#[allow(unused)]
+#[serde(rename = "camelCase")]
+pub struct CurseFile {
+    pub id: i32,
+    pub game_id: i32,
+    pub mod_id: i32,
+    pub is_available: bool,
+    pub display_name: String,
+    pub file_name: String,
+    pub release_type: CurseFileReleaseType,
+    pub file_status: CurseFileStatus,
+    pub hashes: Vec<CurseFileHash>,
+    pub file_date: String,
+    pub file_length: i64,
+    pub download_count: i64,
+    pub file_size_on_disk: i64,
+    pub download_url: String,
+    pub game_versions: Vec<String>,
+    pub sortable_game_versions: Vec<CurseSortableGameVersions>,
+    pub dependencies: Vec<CurseFileDependency>,
+    pub expose_as_alternative:Option<bool>,
+    pub parent_project_file_id:Option<i32>,
+    pub alternate_file_id:Option<i32>,
+    pub is_server_pack:Option<bool>,
+    pub is_early_access_content:Option<bool>,
+    pub early_access_end_date:Option<String>,
+    pub file_fingerprint:i64,
+    pub modules:Vec<CurseFileModule>
+    //TODO FileIndex onward
+}
+#[derive(Clone)]
+pub enum CurseFileReleaseType {
+    Release,
+    Beta,
+    Alpha,
+}
+impl<'de> Deserialize<'de> for CurseFileReleaseType {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        match i32::deserialize(deserializer) {
+            Ok(i) => {
+                if i <= 3 && i >= 1 {
+                    Ok([Self::Release, Self::Beta, Self::Alpha][i as usize - 1].clone())
+                } else {
+                    Err(serde::de::Error::invalid_value(
+                        serde::de::Unexpected::Signed(i as i64),
+                        &"a number [1,3]",
+                    ))
+                }
+            }
+            Err(e) => Err(e),
+        }
+    }
+}
+#[derive(Clone)]
+pub enum CurseFileStatus {
+    Processing,
+    ChangesRequired,
+    UnderReview,
+    Approved,
+    Rejected,
+    MalwareDetected,
+    Deleted,
+    Archived,
+    Testing,
+    Released,
+    ReadyForReview,
+    Deprecated,
+    Baking,
+    AwaitingPublishing,
+    FailedPublishing,
+    Cooking,
+    Cooked,
+    UnderManualReview,
+    ScanningForMalware,
+    ProcessingFile,
+    PendingRelease,
+    ReadyForCooking,
+    PostProcessing,
+}
+impl<'de> Deserialize<'de> for CurseFileStatus {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        match i32::deserialize(deserializer) {
+            Ok(i) => {
+                if i <= 23 && i >= 1 {
+                    Ok([
+                        Self::Processing,
+                        Self::ChangesRequired,
+                        Self::UnderReview,
+                        Self::Approved,
+                        Self::Rejected,
+                        Self::MalwareDetected,
+                        Self::Deleted,
+                        Self::Archived,
+                        Self::Testing,
+                        Self::Released,
+                        Self::ReadyForReview,
+                        Self::Deprecated,
+                        Self::Baking,
+                        Self::AwaitingPublishing,
+                        Self::FailedPublishing,
+                        Self::Cooking,
+                        Self::Cooked,
+                        Self::UnderManualReview,
+                        Self::ScanningForMalware,
+                        Self::ProcessingFile,
+                        Self::PendingRelease,
+                        Self::ReadyForCooking,
+                        Self::PostProcessing,
+                    ][i as usize - 1]
+                        .clone())
+                } else {
+                    Err(serde::de::Error::invalid_value(
+                        serde::de::Unexpected::Signed(i as i64),
+                        &"a number [1,23]",
+                    ))
+                }
+            }
+            Err(e) => Err(e),
+        }
+    }
+}
+#[derive(Deserialize)]
+#[allow(unused)]
+pub struct CurseFileHash {
+    pub value: String,
+    pub algo: CurseHashAlgo,
+}
+#[derive(Clone)]
+pub enum CurseHashAlgo {
+    Sha1,
+    Md5,
+}
+impl<'de> Deserialize<'de> for CurseHashAlgo {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        match i32::deserialize(deserializer) {
+            Ok(i) => {
+                if i >= 1 && i <= 2 {
+                    Ok([Self::Sha1, Self::Md5][i as usize - 1].clone())
+                } else {
+                    Err(serde::de::Error::invalid_value(
+                        serde::de::Unexpected::Signed(i as i64),
+                        &"a number [1,2]",
+                    ))
+                }
+            }
+            Err(e) => Err(e),
+        }
+    }
+}
+#[derive(Deserialize)]
+#[allow(unused)]
+#[serde(rename = "camelCase")]
+pub struct CurseSortableGameVersions {
+    pub game_version_name: String,
+    pub game_version_padded: String,
+    pub game_version: String,
+    pub game_version_release_date: String,
+    pub game_version_type_id: Option<i32>,
+}
+#[derive(Deserialize)]
+#[allow(unused)]
+#[serde(rename = "camelCase")]
+pub struct CurseFileDependency {
+    pub mod_id: i32,
+    pub relation_type: CurseFileRelationType,
+}
+#[derive(Clone)]
+pub enum CurseFileRelationType {
+    EmbeddedLibrary,
+    OptionalDependency,
+    RequiredDependency,
+    Tool,
+    Incomplete,
+    Include,
+}
+impl<'de> Deserialize<'de> for CurseFileRelationType {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        match i32::deserialize(deserializer) {
+            Ok(i) => {
+                if i <= 6 && i >= 1 {
+                    Ok([
+                        Self::EmbeddedLibrary,
+                        Self::OptionalDependency,
+                        Self::RequiredDependency,
+                        Self::Tool,
+                        Self::Incomplete,
+                        Self::Include,
+                    ][i as usize - 1]
+                        .clone())
+                } else {
+                    Err(serde::de::Error::invalid_value(
+                        serde::de::Unexpected::Signed(i as i64),
+                        &"a number [1,6]",
+                    ))
+                }
+            }
+            Err(e) => Err(e),
+        }
+    }
+}
+#[derive(Deserialize)]
+#[allow(unused)]
+pub struct CurseFileModule {
+    pub name:String,
+    pub fingerprint:i64
 }
