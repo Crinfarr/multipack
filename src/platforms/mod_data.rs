7ed3deba758f5060bac0a13a2c062e1456046408
Asher Hoeveler <ahoeveler@apotechgroup.com>
2025-10-06 18:54:15 -0500

    platforms: add mod_data core types and error handling

---
+++ src/platforms/mod_data.rs
@@ -0,0 +1,26 @@
+pub mod curse_mod_data;
+pub mod modrinth_mod_data;
+
+use std::{error::Error, fmt::Display};
+
+#[derive(Debug)]
+pub struct FetchError(String);
+impl Display for FetchError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+impl Error for FetchError {}
+
+#[derive(Default)]
+pub struct ModInfo<T, U> {
+    pub(super) config: T,
+    pub(super) resolved_info: Option<U>,
+    pub sha1: Option<String>,
+    pub file_name: Option<String>,
+    pub(in crate::platforms::mod_data) client: reqwest::Client, //reqwest::Client already uses Arc internally for clones (i am brain damaged)
+    pub(super) resolved: bool,
+}
+pub trait DepResolve {
+    async fn resolve_deps(self) -> Self;
+}
